3c20a5413f3ddb313d4055156953185004dffffa
ryanf <ryanfisher00@outlook.com>
2021-10-17 16:15:01 -0400

    final commit

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -16,14 +16,14 @@ public class Solution42 {
         //declare input file
         String inputFile = "data/exercise42_input.txt";
 
-        //use getInput() to return list of inputFile lines
-        List<String> dataList = app.getInput(inputFile);
+        //use getInput() to return array of inputFile lines
+        String[] dataArr = app.getInput(inputFile);
 
         //use getOutput() to display data formatted
-        app.getOutput(dataList);
+        app.getOutput(dataArr);
     }
 
-    private List getInput(String inputFile) throws Exception {
+    private String[] getInput(String inputFile) throws Exception {
         //create new filereader and bufferedreader instance
         FileReader fw = new FileReader(inputFile);
         BufferedReader br = new BufferedReader(fw);
@@ -33,23 +33,32 @@ public class Solution42 {
         List<String> lines = new ArrayList<>();
 
         //iterate through file and add each line to arraylist
+        while ((inputLine = br.readLine()) != null) {
+            lines.add(inputLine);
+        }
 
-        //close file and return list
-
-        return lines;
+        //close file and return list as array
+        fw.close();
+        return lines.toArray(new String[lines.size()]);
     }
 
-    private void getOutput(List<String> data) throws Exception {
+    private void getOutput(String[] data) throws Exception {
         //create new filewriter and bufferedwriter instance for output.txt
         FileWriter fw = new FileWriter("data/exercise42_output.txt");
         PrintWriter pw = new PrintWriter(fw);
-        //convert list to array for split
+
         //declare new array for split method
-        String[] dataArr = new String[data.size()];
+        String[] dataArr = new String[data.length];
+
+        pw.printf("Last      First     Salary%n--------------------------%n");
 
         //iterate through fileLines array and have it split by the comma into new array
-
-        //print every iteration of the array split
+        for(String s : data) {
+            dataArr = s.split(",", 0);
+            //print every iteration of the array split
+            pw.printf("%-10s%-10s%s%n", dataArr[0], dataArr[1], dataArr[2]);
+        }
+        pw.close();
     }
 }
 
