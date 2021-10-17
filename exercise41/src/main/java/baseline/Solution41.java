3ff098a3025dd987fc30008a711584c776ff1762
ryanf <ryanfisher00@outlook.com>
2021-10-17 15:56:13 -0400

    final commit

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -6,6 +6,7 @@
 package baseline;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution41 {
@@ -20,6 +21,7 @@ public class Solution41 {
         names = app.getInput(inputFile);
 
         //sort list of names in sortInput()
+        Collections.sort(names);
 
         //print out alphabetical list of names
         app.makeOutput(names);
@@ -35,25 +37,33 @@ public class Solution41 {
         List<String> names = new ArrayList<>();
 
         //iterate through file and add each line to arraylist
-
+        while ((inputLine = br.readLine()) != null) {
+            names.add(inputLine);
+        }
 
         //close file and return list
-
+        fw.close();
         return names;
     }
 
     private void makeOutput(List<String> names) throws IOException {
         //declare and convert list to array of listsize
         String[] nameArr = new String[names.size()];
-
+        nameArr = names.toArray(nameArr);
 
         //create new filewriter and bufferedwriter instance for output.txt
         FileWriter fw = new FileWriter("data/exercise41_output.txt");
         PrintWriter pw = new PrintWriter(fw);
 
         //print output through printwriter from name arr
-        //close file
-
+        try {
+            pw.printf("Total of %d names%n-----------------%n", names.size());
+            for (String s : nameArr)
+                pw.printf("%s%n", s);
+            //close file
+        } finally {
+            pw.close();
+        }
     }
 }
 
